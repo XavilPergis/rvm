aba76e47195e459cee9d3bcda63897b90c912bed
Maxwell Duzen <max.duzen@gmail.com>
2018-12-22 15:10:37 -0500

    Documented `Version`

--- src/raw/class.rs
+++ src/raw/class.rs
@@ -249,27 +249,13 @@ impl std::fmt::Display for Access {
     }
 }
 
-// u16 minor_version
-// u16 major_version
-//
-// The values of the minor_version and major_version items are the minor and
-// major version numbers of this class file. Together, a major and a minor
-// version number determine the version of the class file format. If a class
-// file has major version number M and minor version number m, we denote the
-// version of its class file format as M.m. Thus, class file format versions may
-// be ordered lexicographically, for example, 1.5 < 2.0 < 2.1.
-//
-// A Java Virtual Machine implementation can support a class file format of
-// version v if and only if v lies in some contiguous range Mi.0 ≤ v ≤ Mj.m. The
-// release level of the Java SE platform to which a Java Virtual Machine
-// implementation conforms is responsible for determining the range.
-//
-// Oracle's Java Virtual Machine implementation in JDK release 1.0.2 supports
-// class file format versions 45.0 through 45.3 inclusive. JDK releases 1.1.*
-// support class file format versions in the range 45.0 through 45.65535
-// inclusive. For k ≥ 2, JDK release 1.k supports class file format versions in
-// the range 45.0 through 44+k.0 inclusive.
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+/// Version of the class file. Versions are denoted as `M.m` where `M` is the
+/// major version and `m` is the minor version. The version can be ordered
+/// lexicographically. JVM implementations can choose a range of compatible
+/// versions by selecting a minimum major version `Mi`, maximum major version
+/// `Mj`, and a maximum minor version `m`. With these, the JVM can support
+/// versions on the range `Mi.0 <= v <= Mj.m` for some arbitrary `v`.
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct Version {
     pub major: u16,
     pub minor: u16,
